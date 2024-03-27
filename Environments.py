bbe3012d07d5cc4309738beaf2471debb9988aa1
SmartPotato716 <124715659+SmartPotato716@users.noreply.github.com>
2024-03-27 12:55:31 -0400

    Create Environments.py

---
+++ Environments.py
@@ -0,0 +1,16 @@
+! pip install pyabf
+! pip install lmfit
+import matplotlib as plt
+import numpy as np
+import pandas as pd
+import scipy
+import pyabf
+import scipy.stats as stats
+from scipy.optimize import curve_fit
+from lmfit import Model
+import scipy.signal as signal
+import matplotlib.pyplot as plt
+import scipy.fftpack
+import pandas as pd
+import glob
+import os
