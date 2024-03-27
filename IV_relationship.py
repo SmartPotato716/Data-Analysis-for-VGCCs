1b165db7f81360efacd712876b35afc43302eed4
SmartPotato716 <124715659+SmartPotato716@users.noreply.github.com>
2024-03-27 12:58:29 -0400

    Update IV_relationship.py

--- IV_relationship.py
+++ IV_relationship.py
@@ -1,36 +1,3 @@
-def CheckforIV (abf):
-    """
-    Check if the abf file is an IV recording and if the recordinging is complete.
-    
-    Parameters: abf meta data.
-    
-    Returns: 
-        -bool: True if the file is an IV recording and is complete, False otherwise.
-    """
-    
-    abf.setSweep(0, channel = 0)
-    time1 = abf.sweepX
-    voltage1 = abf.sweepC
-    
-    try: 
-        if len(time1) != 6000:
-            return False
-        
-        # Check if the voltage at a specific point is approximately -100
-        # Using round() to round the value and then compare
-        if round(voltage1[2000]) != -100:
-            return False
-        
-        # Check if the total number of sweeps is 18
-        if len(abf.sweepList) != 18:
-            return False
-        
-        # If all conditions are met
-        return True
-    except Exception as e:
-        print(f"Error processing file {file_path}: {e}")
-        return False
-
   def IV_relationship(filepath, start_time, end_time):
     '''
     Perform batch analysis of abf files in a input filepath, generating IV data and plots. 
